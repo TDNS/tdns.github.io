9fef3ff7a5bd96635b259d8a74f096a61df63997
John Savadkuhi <physitime@gmail.com>
2019-01-10 10:20:22 +0330

    update

---
+++ precache-manifest.ecb25752b7c5335e30a02a6f63377800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3a8f115f144c07658c6ec16d878680a",
+    "url": "./static/media/TronLinkLogo.d3a8f115.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a59af2d1301210eb56bd",
+    "url": "./static/js/main.a59af2d1.chunk.js"
+  },
+  {
+    "revision": "2b1bff15d51f6cbccbb0",
+    "url": "./static/js/1.2b1bff15.chunk.js"
+  },
+  {
+    "revision": "a59af2d1301210eb56bd",
+    "url": "./static/css/main.facf9acf.chunk.css"
+  },
+  {
+    "revision": "b01f9a43408c26e307148091d846a305",
+    "url": "./index.html"
+  }
+];
